8d07dcb20615f97c55630a72379e6bf0bd02436c
Morten Kleveland <mortengkleveland@gmail.com>
2016-02-15 22:41:34 +0100

    Initial Commit

---
+++ eq/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  eq
+//
+//  Created by Morten Kleveland on 15.02.2016.
+//  Copyright © 2016 Morten Kleveland. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
---
+++ eq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  eq
+//
+//  Created by Morten Kleveland on 15.02.2016.
+//  Copyright © 2016 Morten Kleveland. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
